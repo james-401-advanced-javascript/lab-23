d8be091fcfee5f494bda81732fb6c7f65ae32259
James Dunn <jamesdu@zillowgroup.com>
2020-02-20 19:50:09 -0800

    Added tcp events for users logging in, logging out, and to send user list

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const server = require("socket.io")();
 
-server.on("connection", client => {
-  client.on("message", socket => {
+const socketPool = {};
+
+server.on("connection", client => {  
+  
+  client.on('username', socket => {
+    socketPool[client.id] = socket.name;
+    // Send this information back to client, to render userlist
+    server.emit('broadcast', {
+      user: socket.name,
+      userGroup: socketPool
+    })
+  });
+
+  client.on('disconnect', () => {
+    if (socketPool[client.id]) {
+      let socketName = socketPool[client.id];
+      delete socketPool[client.id];
+    server.emit('broadcast', {
+      exitMessage: `${socketName} has left the chat.`,
+      userGroup: socketPool
+    })
+  }
+  });
+
+
+    client.on("message", socket => {
     server.emit("broadcast", {
       name: socket.name,
       message: socket.message
     });
   });
+
 });
 
 let PORT = process.env.PORT || 3000;
